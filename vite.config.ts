e2b22cbd3cb467f6928ef0509d01c251a6050ca4
kauatorresfranca <kauatorresfranca2@gmail.com>
2025-09-21 15:04:00 -0300

    adding vercel.json and modifing cite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import compression from 'vite-plugin-compression2';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    compression({
+      algorithms: ['brotliCompress'], // Compressão Brotli
+      exclude: [/\.(br|gz)$/], // Exclui arquivos já comprimidos
+    }),
+    {
+      name: 'purgecss',
+      enforce: 'post' as const,
+      apply: 'build',
+      async transform(code, id) {
+        if (id.endsWith('.css')) {
+          const { PurgeCSS } = await import('purgecss');
+          const purger = new PurgeCSS();
+          const result = await purger.purge({
+            content: ['**/*.tsx', '**/*.html'],
+            css: [{ raw: code }],
+            safelist: ['body', 'html', ':root'],
+          });
+          return {
+            code: result[0].css,
+            map: null,
+          };
+        }
+      },
+    },
+  ],
+  build: {
+    cssMinify: true, // Minifica CSS
+  },
+});
